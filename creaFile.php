1c23d9ffa840e0b42f20df4265d04994ec6f2783
TommyDrazza <99846943+TommyDrazza@users.noreply.github.com>
2022-02-17 00:22:26 +0100

    Create creaFile.php

---
+++ creaFile.php
@@ -0,0 +1,18 @@
+<?php
+
+  echo $_POST;
+
+  /*$filename = '../Filestxt/' . $_REQUEST["q"] . '.txt';
+  $handler = fopen($filename, 'w');
+
+  if (false === $handler) {
+    echo 'false';
+  }
+
+  else {
+    echo 'true';
+  }
+
+  fclose($handler);*/
+
+?>
